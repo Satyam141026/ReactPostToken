b1aa14b4905d55e73fdc0f4530459ca8631ccb47
“Satyam@141026” <satyam@141026@gmail.com>
2022-05-27 11:47:30 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Login from './Pages/Login';
 import Register from './Register';
 
-
+import { BrowserRouter } from "react-router-dom";
 //import Register from './Register';
 import Register2 from './Register2';
+import App1 from './App1';
+import { Nav } from 'react-bootstrap';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Register2 />
-    <Register/>
+    
+<Nav/>
+  <BrowserRouter>
+      <App1/>
+    </BrowserRouter>
     
   </React.StrictMode>
 );
